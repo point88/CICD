6de20db7a6318c16e8ca9d694cbad49e5aade2ab
point88 <queendev2022@gmail.com>
2024-07-13 12:01:30 -0500

    Update CI script

--- simpleHTTP.go
+++ simpleHTTP.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
- 	"net/http"
 )
 
 func main() {
 	log.Print("simplehttp: Enter main()")
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	http.ListenAndServe("0.0.0.0:8000", nil)
 }
 
 // printing request headers/params
 func handler(w http.ResponseWriter, r *http.Request) {
-
-	log.Print("request from address: %q\n", r.RemoteAddr)
-	fmt.Fprintf(w, "Env message = %s\n", os.Getenv("message"))
+	fmt.Fprintf(w, "%s\n", os.Getenv("message"))
 }
